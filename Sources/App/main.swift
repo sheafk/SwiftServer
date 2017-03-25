56b7a1296eee974f71b434bf4fbd61c864c4e6b5
sheafk <sheafureyking@gmail.com>
2017-03-25 15:47:58 -0400

    Hardcorded sample JSON

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -12,6 +12,13 @@ drop.get("/hello") { request in
     return "Hello World!"
 }
 
+drop.get("/people") { request in
+    return try JSON(node: ["people": [
+                                    ["id" : 1234, "name": "Sean", "favoriteCity": "New York"],
+                                    ["id" : 1235, "name": "Tom", "favoriteCity": "Philadelphia"],
+                                    ["id" : 1236, "name": "Sara", "favoriteCity": "Pittburgh"]]
+        ])
+}
 drop.resource("posts", PostController())
 
 drop.run()
